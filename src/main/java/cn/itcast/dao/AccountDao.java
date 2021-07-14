d61bc5ad6fba24932ab23138aa0b402d7b2fc298
YuDongZhang <977872239@qq.com>
2021-07-14 15:11:42 +0800

    6,mybatis在测试类中测试成功

--- src/main/java/cn/itcast/dao/AccountDao.java
+++ src/main/java/cn/itcast/dao/AccountDao.java
@@ -1,6 +1,8 @@
 package cn.itcast.dao;
 
 import cn.itcast.domain.Account;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
@@ -10,8 +12,10 @@ import java.util.List;
 public interface AccountDao {
 
     //查询所有账户
+    @Select("select * from account")
     public List<Account> findAll();
 
     //保存账户信息
+    @Insert("insert into account (name,money) values (#{name},#{money})")
     public void saveAccount(Account account);
 }
