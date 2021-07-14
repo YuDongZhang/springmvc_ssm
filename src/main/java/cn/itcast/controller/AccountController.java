edbe190cd9417a410ed57ae70ff0dd498e736475
YuDongZhang <977872239@qq.com>
2021-07-14 14:44:20 +0800

    5,spring 整合 springmvc 框架成功

--- src/main/java/cn/itcast/controller/AccountController.java
+++ src/main/java/cn/itcast/controller/AccountController.java
@@ -1,5 +1,7 @@
 package cn.itcast.controller;
 
+import cn.itcast.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,9 +12,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/account")
 public class AccountController {
 
+    @Autowired
+    private AccountService accountService;
+
     @RequestMapping("/findAll")
     public String findAll() {
         System.out.println("表现层查询所有信息 AccountController.findAll()执行了...");
+        //调用 service 的方法(这里要有service对象 , 从而要注入service对象)
+        accountService.findAll();
         return "list";
     }
 }
